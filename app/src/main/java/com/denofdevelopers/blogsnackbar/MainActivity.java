e9e7925fad5f5874782941afb7ecf88ea51657af
Viktor Jovanovski <BuktopMKD@gmail.com>
2018-12-26 11:17:18 +0100

    JAVA 8 with Lambda expressions Implementation

--- app/src/main/java/com/denofdevelopers/blogsnackbar/MainActivity.java
+++ app/src/main/java/com/denofdevelopers/blogsnackbar/MainActivity.java
@@ -2,7 +2,6 @@ package com.denofdevelopers.blogsnackbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -33,28 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick({R.id.buttonSnackBar})
     public void onButtonSnackBarClick() {
-        Snackbar.make(rootLayout, "This is SnackBar with button", Snackbar.LENGTH_LONG).setAction("Button", new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Do your stuff
-                // Automatic dismiss when button clicked
-                // Default color of the button text is your colorAccent
-                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
-            }
-        }).show();
+        Snackbar.make(rootLayout, "This is SnackBar with button", Snackbar.LENGTH_LONG).setAction("Button"
+                , view -> Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show()).show();
     }
 
     @OnClick({R.id.customSnackBar})
     public void onCustomSnackBarClick() {
         Snackbar snackbar = Snackbar.make(rootLayout, "This is custom SnackBar", Snackbar.LENGTH_LONG)
-                .setAction("Button", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        // Do your stuff
-                        // Automatic dismiss when button clicked
-                        Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                .setAction("Button", view -> Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show());
         snackbar.setActionTextColor(Color.WHITE);
         View snackBarView = snackbar.getView();
         TextView textView = snackBarView.findViewById(android.support.design.R.id.snackbar_text);
